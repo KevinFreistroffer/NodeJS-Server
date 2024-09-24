7f24aacb3e8c670d6fe00cb89d7763e0936fa765
Kevin Freistroffer <Kevin.freistroffer@gmail.com>
2024-09-24 08:12:41 -0700

    update package json

--- src/pm2.config.js
+++ src/pm2.config.js
@@ -3,6 +3,12 @@ module.exports = {
     {
       name: "server",
       script: "./src/server.ts",
+      env_production: {
+        NODE_ENV: "production",
+      },
+      env_development: {
+        NODE_ENV: "development",
+      },
     },
   ],
 };
