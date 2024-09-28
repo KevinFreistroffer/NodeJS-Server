1b642aee51a2c589cd1bfcff1762c7df8a53f0be
Kevin Freistroffer <Kevin.freistroffer@gmail.com>
2024-09-28 10:01:41 -0700

    edits email-available responses

--- src/router/routes/user/email-available.ts
+++ src/router/routes/user/email-available.ts
@@ -3,10 +3,12 @@
 import * as express from "express";
 import { body } from "express-validator";
 import { responses as userResponses } from "../../../defs/responses/user";
+import { responses as genericResponses } from "../../../defs/responses/generic";
 import { IResponse } from "../../../defs/responses/generic";
 import { IEmailAvailableResponse } from "../../../defs/responses/user";
 import { findOneByEmail } from "../../../operations/user_operations";
 import { validationResult } from "express-validator";
+import { statusCodes } from "../../../defs/responses/status_codes";
 import { handleCaughtErrorResponse } from "../../../utils";
 
 const router = express.Router();
@@ -16,22 +18,22 @@ router.post(
   body("email").isEmail().bail().escape(),
   async (
     req: express.Request,
-    res: express.Response<IEmailAvailableResponse>
+    res: express.Response<IEmailAvailableResponse | IResponse>
   ) => {
     try {
       const validatedErrors = validationResult(req).array();
       if (validatedErrors.length) {
-        return res.status(422).json();
+        return res
+          .status(statusCodes.missing_body_fields)
+          .json(genericResponses.missing_body_fields());
       }
 
       const email = req.body.email;
       const doc = await findOneByEmail(email);
 
-      if (!doc) {
-        return res.json(userResponses.email_available(true));
-      }
-
-      return res.json(userResponses.email_available(false));
+      return res
+        .status(statusCodes.success)
+        .json(userResponses.email_available(!doc));
     } catch (error) {
       return handleCaughtErrorResponse(error, req, res);
     }
